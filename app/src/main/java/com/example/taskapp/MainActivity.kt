cafa2824f2a3085d7ab00e2a2dbfd4f0b9f97339
Eugene <eugenemgithub@gmail.com>
2024-02-03 13:37:28 +0300

    Finished

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -43,9 +44,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
@@ -105,6 +109,16 @@ fun StatisticUI() {
             verticalAlignment = Alignment.CenterVertically
         ){
 
+            Column(modifier = Modifier
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(10.dp)
+                        .clip(CircleShape)
+                        .background(Color.Red)
+                )
+            }
+
             Box(
                 modifier = Modifier
                     .clip(CircleShape)
@@ -153,7 +167,11 @@ fun StatisticUI() {
                     .clip(shape = RoundedCornerShape(8.dp))
                     .background(Color(0xFFE1E3FA))
                     .padding(horizontal = 10.dp, vertical = 2.dp)
-                    .border(width = 0.dp, color = Color.Transparent, shape = RoundedCornerShape(8.dp))
+                    .border(
+                        width = 0.dp,
+                        color = Color.Transparent,
+                        shape = RoundedCornerShape(8.dp)
+                    )
             ){
                 Text(
                     text = "On Going",
@@ -165,6 +183,121 @@ fun StatisticUI() {
 
 
         }
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+        Text(
+            text = "Statistic",
+            fontFamily = Poppins,
+            fontSize = 16.sp,
+            color = SubTextColor,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            StatisticProgressUI()
+            Spacer(modifier = Modifier.width(12.dp))
+            StatisticIndicatorUI()
+        }
+    }
+}
+
+@Composable
+fun StatisticIndicatorUI() {
+   Column(
+       modifier = Modifier
+           .height(120.dp),
+       verticalArrangement = Arrangement.SpaceBetween
+   ) {
+       IndicatorItemUI(text = "Finish on time")
+       IndicatorItemUI(color = SecondaryColor, text = "Past the deadline")
+       IndicatorItemUI(color = Color(0xFFE3E5E7), text = "Still ongoing")
+   }
+}
+
+@Composable
+fun IndicatorItemUI(text: String, color: Color = PrimaryColor) {
+ Row(){
+     Icon(
+         painter = painterResource(id = R.drawable.ic_circle),
+         contentDescription = "",
+         tint = color,
+         modifier = Modifier.size(20.dp)
+     )
+     Spacer(modifier = Modifier.width(12.dp))
+     Text(
+         text = text,
+         fontFamily = Poppins,
+         fontSize = 12.sp,
+         color = Color(0xFF818181),
+         fontWeight = FontWeight.Normal
+     )
+
+ }
+}
+
+@Composable
+fun StatisticProgressUI(primaryPercentage: Float = 60f,secondaryPercentage:Float=15f) {
+    Box(
+        modifier = Modifier
+            .size(120.dp) ,
+        contentAlignment = Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(100.dp)
+        ) {
+
+            drawCircle(
+                brush = SolidColor(Color(0xFFE3E5E7)),
+                radius = size.width /2,
+//                style = Fill
+                style = Stroke(34f)
+            )
+
+                val convertedPrimaryValue = (primaryPercentage / 100) * 360
+            val convertedSecondaryValue = ((secondaryPercentage / 100) * 360)+convertedPrimaryValue
+            drawArc(
+                brush = SolidColor(SecondaryColor),
+                startAngle = -90f,
+                sweepAngle = convertedSecondaryValue,
+                useCenter = false,
+                style = Stroke(34f, cap = StrokeCap.Round)
+            )
+            drawArc(
+                brush = SolidColor(PrimaryColor),
+                startAngle = -90f,
+                sweepAngle = convertedPrimaryValue,
+                useCenter = false,
+                style = Stroke(34f, cap = StrokeCap.Round)
+            )
+        }
+
+        val annotatedString2 = AnnotatedString.Builder("${(primaryPercentage+secondaryPercentage).toInt()}%\nDone")
+            .apply {
+                addStyle(
+                    SpanStyle(
+                        color = SubTextColor,
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Normal
+                    ), 4, 8
+                )
+            }
+
+        Text(
+            text = annotatedString2.toAnnotatedString(),
+            fontFamily = Poppins,
+            fontSize = 20.sp,
+            color = PrimaryTextColor,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            lineHeight = 10.sp
+        )
     }
 }
 
