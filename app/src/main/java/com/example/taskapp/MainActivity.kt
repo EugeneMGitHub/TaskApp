2a31e44cdc307ace2af7dc38e8e4a31191303548
Eugene <eugenemgithub@gmail.com>
2024-02-03 12:36:45 +0300

    Second Commit

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.taskapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
@@ -27,22 +29,29 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,6 +59,7 @@ import androidx.core.content.ContextCompat
 import com.example.taskapp.ui.theme.Poppins
 import com.example.taskapp.ui.theme.PrimaryColor
 import com.example.taskapp.ui.theme.PrimaryTextColor
+import com.example.taskapp.ui.theme.SecondaryColor
 import com.example.taskapp.ui.theme.SubTextColor
 import com.example.taskapp.ui.theme.TaskAppTheme
 
@@ -107,10 +117,15 @@ fun TaskCardUI() {
    ){
         Row(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(20.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.CenterVertically
         ){
-            Column(){
+            Column(
+//                modifier = Modifier.weight(1f)
+
+            ){
                 Text(
                     text = "Daily Task",
                     fontFamily = Poppins,
@@ -139,7 +154,8 @@ fun TaskCardUI() {
                     fontFamily = Poppins,
                     fontSize = 13.sp,
                     color = Color(0xFF292D32),
-                    fontWeight = FontWeight.Normal
+                    fontWeight = FontWeight.Normal,
+                    lineHeight = 16.sp
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 Button(
@@ -164,8 +180,70 @@ fun TaskCardUI() {
                     )
                 }
             }
+//            Spacer(modifier = Modifier.weight(1f))
+            ProgressBarUI(percentage = 67f)
         }
    }
+}
+
+@Composable
+fun ProgressBarUI(percentage: Float) {
+Box(
+    modifier = Modifier.size(120.dp),
+    contentAlignment = Center
+){
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(6.dp)
+    ){
+            drawCircle(
+                brush = SolidColor(Color(0xFFE3E5E7)),
+                radius = size.width / 2,
+                style = Stroke(26f)
+            )
+            //        drawCircle(
+            //            brush = Brush.sweepGradient(listOf(Color.Magenta, Color.Red)),
+            //            radius = size.width / 2,
+            //            style = Stroke(26f)
+            //        )
+
+        val convertedValue = (percentage / 100) * 360
+
+        drawArc(
+            brush = Brush.linearGradient(
+                colors = listOf(SecondaryColor, PrimaryColor)
+            ),
+            startAngle = -90f,
+            sweepAngle = convertedValue,
+            useCenter = false,
+            style = Stroke(width = 26f, cap = StrokeCap.Round)
+        )
+    }
+
+    val annotatedString2 = AnnotatedString.Builder(
+        "${percentage.toInt()}%\nDone"
+    ).apply {
+        addStyle(
+            SpanStyle(
+                color = SecondaryColor,
+                fontSize = 8.sp,
+                fontWeight = FontWeight.Normal
+            ), 4,8
+        )
+    }
+
+    Text(
+        text = annotatedString2.toAnnotatedString(),
+        fontFamily = Poppins,
+        fontSize = 14.sp,
+        color = PrimaryTextColor,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        style = LocalTextStyle.current.copy(lineHeight = 10.sp)
+    )
+
+}
 }
 
 @Composable
